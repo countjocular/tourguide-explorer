ebbd6121ba414cdded822d92dfa31813f1c80187
Count Jocular <i.am.count.jocular@gmail.com>
2010-04-04 13:26:08 +0100

    Added Ghostlands.

---
+++ Horde_Explorer/Ghostlands.lua
@@ -0,0 +1,27 @@
+TourGuide:RegisterGuide("Explore Eastern Kingdoms - Ghostlands (north)", "Explore Eastern Kingdoms - Ghostlands (south)", "Horde", function()
+return [[
+F Tranquillien |N| Remember to check off any zone you've already discovered before you start!|
+R Tranquillien |N|(46, 32)| |Z|Ghostlands|
+R Suncrown Village |N|(61, 12)|
+R Elrendar Crossing |N|(48, 11)|
+R Goldenmist Village |N|(26, 16)|
+R Windrunner Village |N|(18, 43)|
+R Sanctum of the Moon |N|(33, 35)|
+R Sanctum of the Sun |N|(55, 48)|
+R Dawnstar Spire |N|(79, 21)|
+R Farstrider Enclave |N|(72, 32)|
+]]
+end)
+
+TourGuide:RegisterGuide("Explore Eastern Kingdoms - Ghostlands (south)", nil, "Horde", function()
+return [[
+R Howling Ziggurat |N|Remember to check off any zone you've already discovered before you start! (40, 49)| |Z|Ghostlands|
+R Bleeding Ziggurat |N|(34, 47)|
+R Windrunner Spire |N|(15, 56)|
+R Deatholme |N|(35, 72)|
+R Thalassian Pass |N|(46, 75)|
+R Amani Pass |N|(75, 63)|
+R Zeb'Nowa |N|(65, 60)|
+]]
+end)
+
