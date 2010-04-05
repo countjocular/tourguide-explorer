f0ac8f945423ca666f2d30e94c1cb8d04b1f50c2
Count Jocular <i.am.count.jocular@gmail.com>
2010-04-05 19:19:25 +0100

    Modifications to Ghostlands, based on playtesting.

--- Horde_Explorer/Ghostlands.lua
+++ Horde_Explorer/Ghostlands.lua
@@ -1,27 +1,33 @@
 TourGuide:RegisterGuide("Explore Eastern Kingdoms - Ghostlands (north)", "Explore Eastern Kingdoms - Ghostlands (south)", "Horde", function()
 return [[
-F Tranquillien |N| Remember to check off any zone you've already discovered before you start!|
+N Welcome to Ghostlands (North) |N|Remember to check off any zone you've already discovered before you start!|
+F Tranquillien
 R Tranquillien |N|(46, 32)| |Z|Ghostlands|
 R Suncrown Village |N|(61, 12)|
 R Elrendar Crossing |N|(48, 11)|
+N Down the hill towards Goldenmist Village |N| Head down around (36,26)|
 R Goldenmist Village |N|(26, 16)|
+N Down the hill towards Windrunner Village |N| Head down around (22,22)|
 R Windrunner Village |N|(18, 43)|
 R Sanctum of the Moon |N|(33, 35)|
 R Sanctum of the Sun |N|(55, 48)|
-R Dawnstar Spire |N|(79, 21)|
 R Farstrider Enclave |N|(72, 32)|
+R Dawnstar Spire |N|(79, 21)|
 ]]
 end)
 
 TourGuide:RegisterGuide("Explore Eastern Kingdoms - Ghostlands (south)", nil, "Horde", function()
 return [[
-R Howling Ziggurat |N|Remember to check off any zone you've already discovered before you start! (40, 49)| |Z|Ghostlands|
+N Welcome to Ghostlands (South) |N|Remember to check off any zone you've already discovered before you start!|
+R Howling Ziggurat |N|(40, 49)| |N|No need to go into the Ziggurats. Just stand outside. |Z|Ghostlands|
 R Bleeding Ziggurat |N|(34, 47)|
-R Windrunner Spire |N|(15, 56)|
-R Deatholme |N|(35, 72)|
+R Windrunner Spire |N|You may have to head right to the top of the tower to trigger this one. (15, 56)|
+R Deatholme |N|Standing on the crags around (33.1,69.8) should be enough to trigger it.|
+N The road to Thalassian Pass |N|Head up at around (44.0,63.9)|
 R Thalassian Pass |N|(46, 75)|
+N Back the way you came |N| Back down the path, to about (45,64)|
 R Amani Pass |N|(75, 63)|
-R Zeb'Nowa |N|(65, 60)|
+N Zeb'Nowa flightpoint |N|Zeb'Nowa isn't part of the achievement, but it's the nearest flightpoint from here. You might want to head that way anyway. (65, 60)|
 ]]
 end)
 
