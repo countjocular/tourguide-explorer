7520211473a5b8c8c2633c7cd97f709fa0662f39
Count Jocular <i.am.count.jocular@gmail.com>
2010-04-05 13:06:16 +0100

    Added Western Plaguelands.

---
+++ Horde_Explorer/Western_Plaguelands.lua
@@ -0,0 +1,20 @@
+TourGuide:RegisterGuide("Explore Eastern Kingdoms - Western Plaguelands", nil, "Horde", function()
+return [[
+F Thondroril River |N|Remember to check off any zone you've already discovered before you start!|
+R Thondroril River |N|(69, 47)| |Z|Western Plaguelands|
+R Gahrron's Withering |N|(62, 58)|
+R Darrowmere Lake |N|(60, 73)|
+R Caer Darrow |N|(68, 78)|
+R Sorrow Hill |N|(51, 78)|
+R The Writhing Haunt |N|(52, 66)|
+R Ruins of Andorhal |N|(43, 69)|
+R The Bulwark |N|(27, 57)|
+R Felstone Field |N|(36, 56)|
+R Dalson's Tears |N|(46, 53)|
+R Northridge Lumber Camp |N|(46, 36)|
+N Path to Hearthglen |N|The path to Hearthglen starts around (51,42)|
+R Hearthglen |N|(44, 16)|
+R The Weeping Cave |N|(65, 40)|
+]]
+end)
+
